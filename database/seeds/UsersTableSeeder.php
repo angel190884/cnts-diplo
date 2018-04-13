dd13e4fa2b6e270ff18d8c26f88e99f48671532f
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-04-13 16:06:35 -0500

    se agregaron usuarios al UserTableSeeder para test

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -29,7 +29,32 @@ class UsersTableSeeder extends Seeder
         ]);
         $user->assignRole('admin');
 
-        for ($i = 0; $i < 4; ++$i) {
+        $user=factory(User::class)->create([
+            'name' => 'name teacher',
+            'last_name' => 'lastname teacher',
+            'email' => 'teacher@teacher.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+        $user->assignRole('teacher');
+
+        $user=factory(User::class)->create([
+            'name' => 'name student',
+            'last_name' => 'lastname student',
+            'email' => 'student@student.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+        $user->assignRole('student');
+
+        $user=factory(User::class)->create([
+            'name' => 'name authenticated',
+            'last_name' => 'lastname authenticated',
+            'email' => 'authenticated@authenticated.com',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+        $user->assignRole('authenticated');
+
+
+        for ($i = 0; $i < 3; ++$i) {
             $user=factory(User::class)->create();
             $user->assignRole('teacher');
         }
