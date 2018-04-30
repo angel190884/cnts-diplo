2fd18a2212f58a92926219ca9cdd24ab4e94193f
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-04-30 13:12:26 -0500

    se modifico el menu de admin

--- resources/views/admin/top-menu.blade.php
+++ resources/views/admin/top-menu.blade.php
@@ -1,9 +1,31 @@
 <li class="nav-item">
-    <a class="nav-link" href="#">admin</a>
+    <a class="nav-link" href="{{ route('home') }}">Inicio</a>
 </li>
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
+        Diplomados
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">listar</a>
+        <a class="dropdown-item" href="#">editar</a>
+        <a class="dropdown-item" href="#">fechas</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">alumnos</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">emitir diploma</a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Inscripciones
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">revizar documentación</a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Calificaciones
     </a>
     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
         <a class="dropdown-item" href="#">Action</a>
@@ -12,6 +34,23 @@
         <a class="dropdown-item" href="#">Something else here</a>
     </div>
 </li>
-<li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Convocatorias
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">emitir</a>
+        <a class="dropdown-item" href="#">dar de baja</a>
+    </div>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Comentarios/Quejas
+    </a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Something else here</a>
+    </div>
 </li>
