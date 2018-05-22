11f477498aa021d8aff0068e649b8bd378e08cf7
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-05-22 00:08:13 -0500

    se agrega css para menu de 3 niveles y se agregan menus de muestra

--- resources/views/student/top-menu.blade.php
+++ resources/views/student/top-menu.blade.php
@@ -1,17 +1,149 @@
 <li class="nav-item">
-    <a class="nav-link" href="#">student</a>
+    <a class="nav-link" href="{{ route('home') }}">Inicio</a>
 </li>
 <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
+    <a class=" nav-link dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Diplomado
     </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="#">Something else here</a>
-    </div>
-</li>
-<li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
+    <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+        <li class="dropdown-submenu">
+            <a  class="dropdown-item" tabindex="-1" href="#">Modulo Introductorio</a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Introducción</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Programas de educación a distancia</a></li>
+                        <li class="dropdown-item"><a href="#">Módulos</a></li>
+                        <li class="dropdown-item"><a href="#">Papel del asesor</a></li>
+                        <li class="dropdown-item"><a href="#">Papel del tutor</a></li>
+                        <li class="dropdown-item"><a href="#">Evaluación</a></li>
+                        <li class="dropdown-item"><a href="#">Guías y principios para práctica transfusional segura</a></li>
+                        <li class="dropdown-item"><a href="#">Objetivos del módulo</a></li>
+                        <li class="dropdown-item"><a href="#">Planificación del estudio</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Profesionalismo</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Papel del personal de enfermería en la donación de sangre</a></li>
+                        <li class="dropdown-item"><a href="#">Papel del personal técnico en los servicios de Medicina transfusional</a></li>
+                        <li class="dropdown-item"><a href="#">Confidencialidad</a></li>
+                        <li class="dropdown-item"><a href="#">Conducta y vestimenta</a></li>
+                        <li class="dropdown-item"><a href="#">Organizaciones profesionales</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Procediminetos de Seguridad</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Responsabilidad por la seguridad</a></li>
+                        <li class="dropdown-item"><a href="#">Identificación de los riesgos potenciales</a></li>
+                        <li class="dropdown-item"><a href="#">Ropa y equipos protectores</a></li>
+                        <li class="dropdown-item"><a href="#">Envío del material desde las unidades móviles hasta el banco de sangre</a></li>
+                        <li class="dropdown-item"><a href="#">Envío de muestras</a></li>
+                        <li class="dropdown-item"><a href="#">Eliminación segura de residuos</a></li>
+                        <li class="dropdown-item"><a href="#">Procedimientos de desinfección</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Calidad y garantía de calidad</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 1.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 1.2.2</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Conservación correcta de la sangre y plasma</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 1.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 1.2.2</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Mantenimiento de la cedena de frio</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 1.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 1.2.2</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Control de existencias</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 1.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 1.2.2</a></li>
+                    </ul>
+                </li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">Plan de acción</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 1.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 1.2.2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a  class="dropdown-item" tabindex="-1" href="#">Modulo Uno</a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.1</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.1.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.1.2</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.2</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.2.2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a  class="dropdown-item" tabindex="-1" href="#">Modulo Dos</a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.1</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.1.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.1.2</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.2</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.2.2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li class="dropdown-submenu">
+            <a  class="dropdown-item" tabindex="-1" href="#">Modulo Tres</a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.1</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.1.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.1.2</a></li>
+                    </ul>
+                </li>
+                <li class="dropdown-submenu">
+                    <a class="dropdown-item" href="#">SubCapitulo 2.2</a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a href="#">Tema 2.2.1</a></li>
+                        <li class="dropdown-item"><a href="#">Tema 2.2.2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    </ul>
 </li>
