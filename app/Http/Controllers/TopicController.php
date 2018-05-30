dbc4021a11fa16f70f17aa3a9738fc987d842ae5
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-05-30 14:32:54 -0500

    se agrego metodo show a Topic

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Module;
 use App\Topic;
+use Auth;
 use Illuminate\Http\Request;
+use Log;
 
 class TopicController extends Controller
 {
@@ -44,9 +47,17 @@ class TopicController extends Controller
      * @param  \App\Topic  $topic
      * @return \Illuminate\Http\Response
      */
-    public function show(Topic $topic)
+    public function show($slug)
     {
-        //
+        $topic = Topic::where('slug', '=', $slug)->firstOrFail();
+
+        if (Auth::user()->hasAnyRole('student','teacher'))
+        {
+            $modules=Module::where('course_id','=',Auth::user()->course_id)->get();
+            return view('student.showContent',compact('topic','modules'));
+        }
+        Log::error('un usuario quiso ver una url que no le corresponde | '.'user:'. Auth::user()->id);
+        return abort(403, 'Unauthorized action.');
     }
 
     /**
