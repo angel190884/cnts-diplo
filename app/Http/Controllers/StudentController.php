bf826db2f5c80b0d66d1edc592a9c28eefb517ef
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-06-04 16:42:09 -0500

    se agrego middleware auth a StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $full_name=$request->get('full_name');
