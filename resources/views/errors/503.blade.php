75981d379b52572cce9957136189335fa911d025
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-06-29 14:38:16 -0500

    se agrego vista de mantenimiento error 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<title>Diplomado - En Mantenimiento</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+</style>
+
+<article>
+    <h1>&iexcl;En construcción!</h1>
+    <div>
+        <p>En breve el sitio del diplomado estara activo pedimos disculpas por los incovenientes causados ¡pronto estaremos en línea de nuevo!</p>
+        <p>&mdash; El equipo</p>
+    </div>
+</article>
