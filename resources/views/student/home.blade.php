ab6725d5c76458d916e7da9bde407451ac9763a9
Angel Daniel Peregrino Jaurez <angel190884@gmail.com>
2018-07-04 16:18:13 -0500

    se modifica home student para que muestre unos accesos directos asi como la tabla de prueba

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -16,20 +16,20 @@
                         <tbody>
                         <tr>
                             <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
+                            <td>uno</td>
+                            <td>19-05-2018</td>
                             <td>8</td>
                         </tr>
                         <tr>
                             <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
+                            <td>dos</td>
+                            <td>19-05-2018</td>
                             <td>10</td>
                         </tr>
                         <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
+                            <th scope="row">3</th>
+                            <td>tres</td>
+                            <td>19-05-2018</td>
                             <td>7</td>
                         </tr>
                         </tbody>
@@ -37,7 +37,7 @@
                 </div>
                 <div class="panel-footer panel-primary">
                     <p>Las ultimas calificaciones de tus tareas apareceran aqui</p>
-                    <a href="{{ route('activity.index') }}" class="btn btn-sm btn-danger">Actividades</a>
+                    <a href="{{ route('activity.index') }}" class="btn btn-danger btn-lg" role="button"><i class="fas fa-clipboard fa-3x"></i> <br/>Actividades</a>
                 </div>
             </div>
         </div>
@@ -68,7 +68,7 @@
                             <td>9</td>
                         </tr>
                         <tr>
-                            <th scope="row">2</th>
+                            <th scope="row">3</th>
                             <td>Jacob</td>
                             <td>Thornton</td>
                             <td>10</td>
@@ -78,7 +78,7 @@
                 </div>
                 <div class="panel-footer panel-primary">
                     <p>Calificaciones de los foros en los que participaste aqui apareceran.</p>
-                    <a href="{{ route('questions.index') }}" class="btn btn-sm btn-danger">Foros</a>
+                    <a href="{{ route('questions.index') }}" class="btn btn-warning btn-lg" role="button"><i class="far fa-comment fa-3x"></i> <br/>Foros</a>
                 </div>
             </div>
         </div>
@@ -109,7 +109,7 @@
                             <td>8</td>
                         </tr>
                         <tr>
-                            <th scope="row">2</th>
+                            <th scope="row">3</th>
                             <td>Jacob</td>
                             <td>Thornton</td>
                             <td>9</td>
@@ -118,8 +118,8 @@
                     </table>
                 </div>
                 <div class="panel-footer panel-primary">
-                    <p>Los examenes que debes presentar apareceran en esta tabla y por tiempo limitado asi que te recomendamos estar pendiente.</p>
-                    <a href="#" class="btn btn-sm btn-danger">Examenes</a>
+                    <p>Los examenes apareceran en esta tabla y por tiempo limitado asi que te recomendamos estar pendiente.</p>
+                    <a href="#" class="btn btn-info btn-lg" role="button"><i class="far fa-edit fa-3x"></i> <br/>Examenes</a>
                 </div>
             </div>
         </div>
