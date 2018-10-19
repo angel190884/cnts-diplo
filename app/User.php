cde2d4fc1c51231073918db1063fdb116deacf96
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2018-10-19 11:03:23 -0500

    se agregaron comentarios phpcs para estetica de codigo a la clase User

--- app/User.php
+++ app/User.php
@@ -1,15 +1,38 @@
 <?php
 
+/**
+ * Clase encargada de los Usuarios.
+ * 
+ * Esta clase de encargara de la gestion del modelo de User se especificaran 
+ * las relaciones con los demas modelos.
+ * 
+ * PHP version 7.1.5
+ * 
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ * 
+ * @category User
+ * @package  Cnts-diplo
+ * @author   Adx Software S.A de C.V. <softwareadx@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version  Release: "GIT:1.0"
+ * @link     https://diplomado-cnts.net/
+ */
 namespace App;
 
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
+// phpcs:disable
 class User extends Authenticatable implements MustVerifyEmail
 {
+    // phpcs:enable
     use Notifiable, HasRoles;
 
     /**
@@ -30,27 +53,49 @@ class User extends Authenticatable implements MustVerifyEmail
         'password', 'remember_token',
     ];
 
-    //RELACIONES
-
+    /**
+     * Relation user->course()
+     * 
+     * @return Response
+     */
     public function course()
     {
         return $this->belongsTo('App\Course');
     }
 
+    /**
+     * Relation user->activities()
+     * 
+     * @return Response
+     */
     public function activities()
     {
-        return $this->belongsToMany('App\Activity')->withTimestamps()->withPivot(['score','file_activity']);
+        return $this->belongsToMany('App\Activity')
+            ->withTimestamps()
+            ->withPivot(['score','file_activity']);
     }
 
+    /**
+     * Relation user->questions()
+     * 
+     * @return Response
+     */
     public function questions()
     {
-        return $this->belongsToMany('App\Question')->withTimestamps()->withPivot(['score']);
+        return $this->belongsToMany('App\Question')
+            ->withTimestamps()
+            ->withPivot(['score']);
     }
 
-    //SCOPE
+    /*
+    |--------------------------------------------------------------------------
+    | Scopes// phpcs:disable
+    |--------------------------------------------------------------------------
+    */
+    // phpcs:disable
     public function scopeRegistered($query)
     {
-        return $query->where('email_verified_at','!=',null);
+        return $query->where('email_verified_at', '!=', null);
     }
 
     public function scopeActive($query)
@@ -60,70 +105,102 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function scopeInscribed($query)
     {
-        return $query->where('course_id','!=',null);
+        return $query->where('course_id', '!=', null);
     }
 
-    public function scopeVoucherSend($query, $voucher_send)
+    public function scopeVoucherSend($query, $voucherSend)
     {
-        if ($voucher_send)
-        {
-            return $query->where('file_voucher','!=',null);
+        if ($voucherSend) {
+            return $query->where('file_voucher', '!=', null);
         }
     }
 
     public function scopeCoursePaid($query, $paid)
     {
-        if ($paid)
-        {
-            return $query->where('file_paid_voucher','!=',null);
+        if ($paid) {
+            return $query->where('file_paid_voucher', '!=', null);
         }
     }
-
-    public function scopeFullName($query, $full_name)
+    
+    public function scopeFullName($query, $fullName)
     {
-        if ($full_name)
-        {
-            return $query->where('name', 'LIKE', "%$full_name%")->orWhere('last_name', 'LIKE', "%$full_name%");
+        if ($fullName) {
+            return $query->where('name', 'LIKE', "%$fullName%")
+                ->orWhere('last_name', 'LIKE', "%$fullName%");
         }
     }
     public function scopeEmail($query, $email)
     {
-        if ($email)
-        {
+        if ($email) {
             return $query->where('email', 'LIKE', "%$email%");
         }
     }
-
+ 
     public function scopeCourseFilter($query, $courseId)
     {
-        if ($courseId)
-        {
+        if ($courseId) {
             return $query->where('course_id', '=', $courseId);
         }
     }
+    // phpcs:enable
+    /*
+    |--------------------------------------------------------------------------
+    | Getters
+    |--------------------------------------------------------------------------
+    */
 
-    //getters
+    /**
+     * Getter para consultar el atributo "fullName"
+     * 
+     * @return fullName
+     */
     public function getFullNameAttribute()
     {
         return strtoupper("{$this->name} {$this->last_name}");
     }
 
+    /**
+     * Getter para consultar el atributo "linkCedula"
+     * 
+     * @return linkCedula
+     */
     public function getLinkCedulaAttribute()
     {
         return "http://cedula.buholegal.com/{$this->cedula}/";
     }
 
+    /**
+     * Getter para consultar el atributo "dateInscriptionFormatBasic"
+     * 
+     * @return dateInscriptionFormatBasic
+     */
     public function getDateInscriptionFormatBasicAttribute()
     {
-        $dt=Carbon::parse($this->date_inscription);
-        return $dt->format('d-m-Y  -- h:m');
+        $dateTime=new Carbon();
+        $dateTime->parse($this->date_inscription);
+        return $dateTime->format('d-m-Y  -- h:m');
     }
 
+    /**
+     * Getter para consultar el atributo "address"
+     * 
+     * @return address
+     */
     public function getAddressAttribute()
     {
-        $address = "$this->calle col. $this->colonia CP.$this->cp municipio $this->municipio, $this->entidad, México.";
+        $address = "$this->calle "
+            . "col. $this->colonia "
+            . "CP.$this->cp "
+            . "municipio $this->municipio, "
+            . "$this->entidad, México.";
         return $address;
     }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions
+    |--------------------------------------------------------------------------
+    */
 
     /**
      * Send the email verification notification.
@@ -134,4 +211,4 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         $this->notify(new Notifications\VerifyEmail);
     }
-}
+}//end class
